4c287afd46b0e6ec168b3fb7e4a0520b7f2bdbb5
Dragan Milchevski <dragan.milcevski@gmail.com>
2018-12-05 15:27:16 +0100

    Adding mvn compile to the script.

--- scripts/driver/run_pipeline.sh
+++ scripts/driver/run_pipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mvn compile
+
 export MAVEN_OPTS="-Xmx8G"
 args=(${@// /\\ })
 mvn exec:java -Dexec.mainClass='de.mpg.mpi_inf.ambiversenlu.nlu.entitylinking.run.UimaCommandLineDisambiguator' -Dorg.postgresql.forcebinary=true -Dexec.args="${args[*]}"
