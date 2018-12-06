1a36a83b96fbe55fef206955cecd1ce35fefaef5
Dragan Milchevski <dragan.milcevski@gmail.com>
2018-12-06 11:39:14 +0100

    Increasing the memory requirements.

--- scripts/driver/run_pipeline.sh
+++ scripts/driver/run_pipeline.sh
@@ -2,6 +2,6 @@
 
 mvn compile
 
-export MAVEN_OPTS="-Xmx8G"
+export MAVEN_OPTS="-Xmx12G"
 args=(${@// /\\ })
 mvn exec:java -Dexec.mainClass='de.mpg.mpi_inf.ambiversenlu.nlu.entitylinking.run.UimaCommandLineDisambiguator' -Dorg.postgresql.forcebinary=true -Dexec.args="${args[*]}"
